d4f2a29d2e28ba8ddd6df019f535ac13bca95326
Jing Yi Jax Chua <s10196708@connect.np.edu.sg>
2021-01-29 09:39:59 +0800

    Notification popup when chat ends and textbox will be disabled

--- FSD_P2_T02_Group2/FSD_P2_T02_Group2/DAL/UserDAL.cs
+++ FSD_P2_T02_Group2/FSD_P2_T02_Group2/DAL/UserDAL.cs
@@ -231,32 +231,23 @@ namespace FSD_P2_T02_Group2.DAL
             cmd.ExecuteNonQuery();
             conn.Close();
         }
-        public List<CounselSession> getSession(int userid)
+        public int getSessions()
         {
             SqlCommand cmd = conn.CreateCommand();
-
-            cmd.CommandText = @"SELECT * FROM CounselSessionView WHERE UserID = @user";
-            cmd.Parameters.AddWithValue("@user", userid);
+            int? count = 0;
+            cmd.CommandText = @"SELECT COUNT(SessionID) FROM PendingCounsellingSession";
             conn.Open();
             SqlDataReader reader = cmd.ExecuteReader();
-            List<CounselSession> cList = new List<CounselSession>();
             if (reader.HasRows)
             {
                 while (reader.Read())
                 {
-                    cList.Add(
-                    new CounselSession
-                    {
-                        UserID = userid,
-                        CounsellorID = reader.GetInt32(1),
-                        CName = reader.GetString(2),
-                        roomName = Convert.ToString(reader.GetInt32(1)) +'-'+ Convert.ToString(userid)
-                    });
+                    count = !reader.IsDBNull(0) ? (int?)reader.GetInt32(0) : null;
                 }
             }
             reader.Close();
             conn.Close();
-            return cList;
+            return count.Value;
         }
         public async Task sendMessage(User user, ChatMessage message, string room)
         {
@@ -299,7 +290,7 @@ namespace FSD_P2_T02_Group2.DAL
         public string OTP(string number)
         {
             const string accountSID = "ACb2940c2a00ccdd56852ced467d8789b2";
-            
+            const string authToken = "";
             // Initialize the TwilioClient.
             TwilioClient.Init(accountSID, authToken);
             string randNum = "";
